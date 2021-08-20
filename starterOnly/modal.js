805e4fdb134b726c2d1e3229c2832d2bc4ab6dae
Benoit Maréchal <benoit.marechal.56@gmail.com>
2021-08-20 19:24:25 +0200

    messages erreur OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,15 +31,12 @@ function closeModal(){
 btnClose.addEventListener("click", closeModal);
 //FIN close modal
 
+//Validation formulaire
 
 
-
-//liste des elements du formulaire:
-let fields = ["firstName", 'lastName', 'email', 'quantity', 'location', 'conditions', 'nextEventsMailAgreement'];
 
 
 // variables de validité des champs
-
  var firstNameValidity=false;
  var lastNameValidity=false;
  var emailValidity=false;
@@ -48,22 +45,22 @@ let fields = ["firstName", 'lastName', 'email', 'quantity', 'location', 'conditi
  var conditionsValidity= true;
  var nextEventsMailAgreementValidity= false;
  var globalValidity=false;
-
- 
- 
+// FIN variables de validité des champs
 
 
- 
-
-
-
-
-//definition fonction "plus de deux caractères, lettres seulement" pour prénom (first) et nom (last)
-
+ //REGEXP
 //regexp validation 2 caratères
 var lettre = new RegExp('[a-z]', 'ig');
 
-//fonction de verification de la regexp
+//regexp validation email
+let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+
+//regexp validation quantités de tournois (quantity)
+var quantityRegExp = new RegExp('[0-9]', 'g');
+
+
+//FONCTIONS DE VERIFICATION DES CHAMPS
+//fonction de verification de la regexp LETTRE
 function stringCheck (stringToCheck){
   if ((((stringToCheck.match(lettre))!==null)&&((stringToCheck.length)>2)&&(((stringToCheck.length) - (stringToCheck.match(lettre).length))==0)))
     {    
@@ -72,79 +69,90 @@ function stringCheck (stringToCheck){
     {return false}    
   }  
 
-//application au prenom (first)
+//fonction de verification de la regexp MAIL
+  function mailCheck (mailToCheck){
+    if ((mailToCheck.match(emailRegExp)!==null))
+      {    
+      return true}
+      else 
+      {return false}    
+    }  
+
+//fonction de verification de la regexp QUANTITY
+function quantityCheck (quantityToCheck){
+  if ((quantityToCheck.match(quantityRegExp)!==null) && (quantityToCheck.length<=2)  )
+    {    
+    return true}
+    else 
+    {return false}    
+  }      
+
+  ////fonction de verification des checkboxes LOCATION
+  function checkCheckboxes() {
+    locationValidity=true
+     // console.log("locationValidity  "+ locationValidity)
+      return locationValidity
+  }
+
+  // function genericCheckboxes(anyBox) {
+  //   var ((anyBox)Validity)=true
+  //     console.log((anyBox)+"Validity  "+ (anyBox)Validity)
+  //     return (anyBox)Validity
+  // }
+
+
+
+//ecoute du  prenom (first)
 
 document
 .getElementById("first")
 .addEventListener("input", function(e){
    firstNameValidity = (stringCheck(this.value))
-  console.log("first " + stringCheck(this.value))
+//  console.log("first " + stringCheck(this.value))
   return firstNameValidity  
 });
 
-//application au nom (last)
+//ecoute du  nom (last)
 document
 .getElementById("last")
 .addEventListener("input", function(e){
   lastNameValidity = (stringCheck(this.value))
-  console.log("last  " + stringCheck(this.value)) 
+ // console.log("last  " + stringCheck(this.value)) 
   return lastNameValidity 
 });
 
-
-//regexp validation email
-let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
-
-//application au champ email
+//ecoute de l'email ("email")
 document
 .getElementById("email")
 .addEventListener("input", function(e){
-  if (  (this.value.match(emailRegExp)!==null)) {
-    emailValidity=true
-  } 
-  else{
-    emailValidity=false
-  }
-    console.log ("emailValidity   " + emailValidity)
-    return emailValidity
-  }  
-);
+  emailValidity = (mailCheck(this.value))
+//  console.log("email  " + mailCheck(this.value)) 
+  return emailValidity 
+});
 
-//regexp validation quantités de tournois (quantity)
-var quantityRegExp = new RegExp('[0-9]', 'g');
-//application au champ quantity
+//ecoute de la quantity ("quantity")
 document
 .getElementById("quantity")
 .addEventListener("input", function(e){
-  if (  (this.value.match(quantityRegExp)!==null) && (this.value.length<=2)      ) {
-    quantityValidity=true
-  } 
-  else{
-    quantityValidity=false
-  }
-    console.log ("quantityValidity   " + quantityValidity)
-  }  
-);
+  quantityValidity = (quantityCheck(this.value))
+ // console.log("quantity  " + quantityCheck(this.value)) 
+  return quantityValidity 
+});
 
 
+// //un bouton radio est selectionné (location)
 
-//un bouton radio est selectionné (location)
+// //ARRAY de toutes les LOCATIONS
 var allLocationsArray = document.getElementsByName("location");
 
-function checkCheckboxes() {
-  Array.from(allLocationsArray).some((checkbox) => checkbox.checked)
-  locationValidity=true
-  console.log("locationValidity  "+ locationValidity)
-  return locationValidity
-  ;
-}
-  allLocationsArray.forEach((checkbox) => {
-    checkbox.addEventListener("input", checkCheckboxes);
+//Ecoute des 6 checkboxes et passage à true si une case est cochée
+  allLocationsArray.forEach((elementOfArray) => {
+  elementOfArray.addEventListener("input", function(e){ 
+      checkCheckboxes() 
+    });
   });
-
-  //conditions generales
-
-
+  //conditions generales (obligatoire) et accord mail (facultatif)
+//checkbox1
  document
  .getElementById("checkbox1")
  .addEventListener("input", function(e){
@@ -154,12 +162,12 @@ function checkCheckboxes() {
   else{
     conditionsValidity=false
   }
-  console.log("conditions accepted (mandatory) "  + conditionsValidity)
+ // console.log("conditions accepted (mandatory) "  + conditionsValidity)
   return conditionsValidity
 })
  ;
  
-
+//checkbox2
  document
  .getElementById("checkbox2")
  .addEventListener("input", function(e){
@@ -168,17 +176,18 @@ function checkCheckboxes() {
  }
   else{
      nextEventsMailAgreementValidity=false
-  }
-  
-  console.log("emails accepted (not mandatory)  " + nextEventsMailAgreementValidity)
+  }  
+ // console.log("emails accepted (not mandatory)  " + nextEventsMailAgreementValidity)
   return nextEventsMailAgreementValidity
 })
  ;
 
 
+
+
 //calcul de la validité globale:
 
-//fonction pour disable le bouton si on passe false
+//fonction pour disable le bouton si on lui passe false
 function disableSubmit(disabled) {
   if (disabled) {
     document
@@ -191,11 +200,13 @@ function disableSubmit(disabled) {
   }
 };
 
-//ecouter le changement de global validity + modifier le bouton
- document
-.getElementById("modalForm")
-.addEventListener("input", function(e){
-  let arrayOfImportantValidities=[
+//attraper n'import quel HTML "-validation"
+function getValidation(string){
+  return document.getElementById(string+"-validation")  
+};
+
+function createArrayOfMandatoryValidities(){
+  let arrayOfMandatoryValidities=[
     firstNameValidity,
     lastNameValidity,
     emailValidity,
@@ -203,21 +214,138 @@ function disableSubmit(disabled) {
     locationValidity,
     conditionsValidity,  
   ]
-  globalValidity = !arrayOfImportantValidities.some(x => x === false) 
-  console.log("globalValidity  " +globalValidity)
-  console.log(arrayOfImportantValidities)  
-  
-  console.log("results  " + disableSubmit(globalValidity.value))
-  disableSubmit(globalValidity)
-  return globalValidity;
-  
-});
+  console.log(arrayOfMandatoryValidities)
+  globalValidity = !arrayOfMandatoryValidities.some(x => x === false) 
+   console.log("globalValidity  " +globalValidity)
 
-function getValidation(string){
-  return document.getElementById(string+"-validation")  
+
+
+
+
+
+  return arrayOfMandatoryValidities;
+ };
+
+// function calculateGolbalValidity(){
+//   globalValidity = !arrayOfMandatoryValidities.some(x => x === false) 
+//   console.log("globalValidity  " +globalValidity)   
+
+// };
+
+class Field {
+  constructor(title, validityValue, errorMessage) {
+    this.title = title;
+    this.validityValue = validityValue;
+    this.errorMessage = errorMessage;
+  }
+}
+
+function updateFields(){
+ let fields = [
+new Field ('firstName', firstNameValidity, "Veuillez entrer 2 caractères ou plus pour le champ du prénom."),
+new Field ('lastName', lastNameValidity, "Veuillez entrer 2 caractères ou plus pour le champ du nom."),
+new Field ('email', emailValidity, "Veuillez entrer une adresse email valide."),
+new Field ('quantity', quantityValidity, "Veuillez entrer un nombre entre 0 et 99"),
+new Field ('location', locationValidity, "Veuillez renseigner la ville qui vous intéresse"),
+new Field ('conditions', conditionsValidity, "Vous devez vérifier que vous acceptez les termes et conditions."),
+ ]
+// //  resultatFinal=true;
+// for (let element of fields) { 
+// getValidation(element)
+// if((element.validityValue)=true){
+//   console.log("nope")
+//  }
+//  else{
+//    console.log("yep")
+//  }
+// };
+ 
+//  console.log("resultatFinal" + resultatFinal);
+  
+for (let field of fields) {
+  console.log(getValidation(field.title))
+  if((field.validityValue)==false){
+    (getValidation(field.title)).innerText=field.errorMessage
+  }
+  else{
+    (getValidation(field.title)).innerText=""
+  }
+
 };
 
-console.log(getValidation("location"));
+
+  // console.log(field.title+" XX "+field.validityValue)
+  // if(field.validityValue==true){
+  //   console.log("yep")
+  // }
+  // else{
+  //   console.log("nope");
+    
+  //  getValidation(field.title).innerText="aaaaaa";
+ //   (getValidation(field.title)).innerText="ygtjkh";
+
+//   }
+// }
+console.log(fields);
+return fields;
+};
+
+// getValidation('quantity')
+// .innerText="aaaaaa";
+
+
+
+
+
+//ecouter le changement de global validity + modifier le bouton
+
+
+
+
+ document
+.getElementById("modalForm")
+.addEventListener("input", function(e){
+  createArrayOfMandatoryValidities()
+   updateFields()  
+   //calculateGolbalValidity()
+   disableSubmit(globalValidity) 
+   return globalValidity; 
+  
+
+//autre option à approfondir (tableau))
+});
+
+//FIN ecouter le changement de global validity + modifier le bouton
+
+
+//fonction pour pointer sur la validation de chaque element
+
+// globalValidity=true;
+
+// console.log(element.errorMessage)
+// console.log(getValidation(element))
+// globalValidity=globalValidity && arrayOfMandatoryValidities.validityValue
+// console.log("resultatFinal  " +resultatFinal)
+// };
+
+// console.log("resultatFinal" + resultatFinal);
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -245,8 +373,6 @@ console.log(getValidation("location"));
   
 
   
-
-
 
 
 
